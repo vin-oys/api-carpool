fd404f41e0a62376b60453629dc29dd22c55a57c
vincent-oys <vincent.ong90@gmail.com>
2023-03-10 00:12:00 +0800

    feat: setup gin router and unit test

---
+++ api/server.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/vin-oys/api-carpool/db/sqlc"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
---
+++ db/sqlc/main_test.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:password@localhost:5432/carpool?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to DB: ", err)
+	}
+
+	testQueries = New(conn)
+	os.Exit(m.Run())
+}
