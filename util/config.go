97261a7cc58e36dafbe06e13b622ed2094d6d781
vincent-oys <vincent.ong90@gmail.com>
2023-04-20 22:19:25 +0800

    feat: add login function

--- util/config.go
+++ util/config.go
@@ -1,15 +1,20 @@
 package util
 
-import "github.com/spf13/viper"
+import (
+	"github.com/spf13/viper"
+	"time"
+)
 
 type Config struct {
-	DBDriver      string `mapstructure:"DB_DRIVER"`
-	DBHost        string `mapstructure:"DB_HOST"`
-	DBUser        string `mapstructure:"DB_USER"`
-	DBPassword    string `mapstructure:"DB_PASSWORD"`
-	DBPort        string `mapstructure:"DB_PORT"`
-	DBName        string `mapstructure:"DB_NAME"`
-	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+	DBDriver            string        `mapstructure:"DB_DRIVER"`
+	DBHost              string        `mapstructure:"DB_HOST"`
+	DBUser              string        `mapstructure:"DB_USER"`
+	DBPassword          string        `mapstructure:"DB_PASSWORD"`
+	DBPort              string        `mapstructure:"DB_PORT"`
+	DBName              string        `mapstructure:"DB_NAME"`
+	ServerAddress       string        `mapstructure:"SERVER_ADDRESS"`
+	TokenSymmetricKey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
